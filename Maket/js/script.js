ac88cfe04521b5e104f0ae3d297821ce9009ef47
VadimHome <vadim.a.shkuratov@gmail.com>
2024-03-13 22:49:18 +0300

    Update maket

---
+++ Maket/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("navbar-toggler").addEventListener('click', ev => {
+    const icon = document.getElementById('navbar-toggler-icon');
+    const collapse = document.getElementById('navbarSupportedContent');
+    if(icon.classList.contains("bi-list-nested") && !collapse.classList.contains("show")){
+        icon.classList.remove('bi-list-nested');
+        icon.classList.add("bi-x");
+    }
+    else{
+        icon.classList.remove("bi-x");
+        icon.classList.add('bi-list-nested');
+    }
+});
