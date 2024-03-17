30404b57077c7c38641d94f46dd9e8161fecd6e5
Vadim <vadim.a.shkuratov@gmail.com>
2024-03-17 18:44:37 +0300

    Create asp project

---
+++ BudsFlowers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BudsFlowers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
